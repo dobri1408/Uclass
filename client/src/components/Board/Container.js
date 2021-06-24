f3911aa3d9e9cb1d73a5786ee01e879cae145b08
Ionut <dobriceanionut1408@gmail.com>
2021-06-24 20:46:11 +0300

    Board with pages

--- client/src/components/Board/Container.js
+++ client/src/components/Board/Container.js
@@ -8,18 +8,7 @@ import NavbarProf from '../NavbarProf';
 import {Button} from 'react-bootstrap'
 function Container(){
 
-var pages = new Array();
-pages.push(<Board page={0}/>) 
-const [currentPage, setCurrentPage] = useState(0);
-const [boards, setBoards ] = useState(pages);
 
-const newPage = () =>{
-   
-  
-    
-
-}
-console.log(boards);
 return (
         <>
       
