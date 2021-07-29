e509ec097b68f91241b581f468b2777912b2ecf3
Ionut <dobriceanionut1408@gmail.com>
2021-07-29 19:41:32 +0300

    ds

--- client/src/components/StudentConnectedPrivateRoute.js
+++ client/src/components/StudentConnectedPrivateRoute.js
@@ -1,36 +1,117 @@
 import React,{useEffect,useState} from "react"
 import { Route, Redirect } from "react-router-dom"
 import { useAuth } from "./contexts/AuthContext"
-import {data} from '../store/data'
+import {db,auth} from './firebase/firebase';
+import {data, change} from '../store/data';
+import Grid from '@material-ui/core/Grid';
+import CircularProgress from "@material-ui/core/CircularProgress";
+
+function useForceUpdate(){
+  const [value, setValue] = useState(0); // integer state
+  return () => setValue(value => value + 1); // update the state to force render
+}
+
+
 export default function StudentConnectedPrivateRoute({ comp: Component, ...rest }) {
   const { currentUser } = useAuth();
-  const [loading, setLoading] = useState(false)
-useEffect(() => {
-  const timer = setTimeout(() => {
-    setLoading(true);
-    console.log('This will run after 1 second!')
-  }, 3000);
-  return () => clearTimeout(timer);
-}, []);
-  if(!loading) 
-  return <div>Loading...</div>
-else
-  return (
-    <Route
-      {...rest}   
-      render={props => {
-       if(currentUser) {
-     if(data.getState().userData.type ==='student')
+  const forceUpdate = useForceUpdate();
+  console.log(currentUser);
 
-      {  return( 
-        <Component {...props} />) } //student
-    else 
-    return( <Redirect to="/profile" />) //teacher
-      }
-      else
-     ( <Redirect to="/" />)
+// const [loading, setLoading] = useState(false)
+// useEffect(() => {
+
+//   // const timer = setTimeout(() => {
+//   //   setLoading(true);
+//   //   console.log('This will run after 1 second!')
+//   // }, 3000);
+//   // return () => clearTimeout(timer);
+
+
+//   if("userData" in data.getState()) {
+//     setLoading(true);
+//   }
+
+// }, []);
+  // if(!loading) 
+  // return (
+  //   <>
+  //     <Grid container spacing={0}>
+  //       <Grid item xs={6}>
+  //       </Grid>
+  //         <CircularProgress style={{
+  //           width: 500, 
+  //           height: 500, 
+  //           marginTop: 100, 
+  //           color: '#D99152', 
+  //           marginLeft: -250
+  //         }} onMouseMove={forceUpdate}/>
+  //       <Grid item xs={6}>
+  //       </Grid>
+  //     </Grid>
         
-    }}
-    ></Route>
-  )
+  //   </>
+  // )
+  // else
+  //   return (
+  //     <Route
+  //     {...rest}   
+  //     render={  props =>  {
+  //     if(currentUser) {
+        
+  
+  //   if( data.getState().userData.type ==='teacher')
+  
+  //     {   return( 
+  //       <Component {...props} /> )} //teacher
+  //   else 
+  //   return( <Redirect to="/student-profile" />)
+  //     }
+  //     else
+  //   ( <Redirect to="/" />)
+        
+  //   }}
+  //     ></Route>
+  //   )
+
+    if("userData" in data.getState()) {
+      return (
+        <Route
+        {...rest}   
+        render={  props =>  {
+        if(currentUser) {
+          
+    
+      if( data.getState().userData.type ==='student')
+    
+        {   return( 
+          <Component {...props} /> )} //teacher
+      else 
+      return( <Redirect to="/profile" />)
+        }
+        else
+      ( <Redirect to="/" />)
+          
+      }}
+        ></Route>
+      )
+    } else {
+      return(
+      <>
+        <Grid container spacing={0}>
+          <Grid item xs={6}>
+          </Grid>
+            <CircularProgress style={{
+              width: 500, 
+              height: 500, 
+              marginTop: 100, 
+              color: '#D99152', 
+              marginLeft: -250
+            }} onMouseMove={forceUpdate}/>
+          <Grid item xs={6}>
+          </Grid>
+        </Grid>
+      </>
+      ) 
+    }
+
 }
--- client/src/components/lessons/ClassCode.js
+++ client/src/components/lessons/ClassCode.js
@@ -5,12 +5,19 @@ import {data} from '../../store/data';
 import Snackbar from '@material-ui/core/Snackbar';
 import Typography from '@material-ui/core/Typography';
 import MuiAlert from '@material-ui/lab/Alert';
+import Dialog from '@material-ui/core/Dialog';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Slide from '@material-ui/core/Slide';
 
-
+const Transition = React.forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+});
 
 export default function ClassCode(props) {
     const [copied, setCopied] = useState(false);
-
+    const [open, setOpen] = useState(false);
     function Alert(props) {
         return <MuiAlert elevation={6} variant="filled" {...props} />;
     }
@@ -23,6 +30,15 @@ export default function ClassCode(props) {
         if (reason === 'clickaway') {return}
         setCopied(false);
     };
+
+
+    const handleOpen = () => {
+        setOpen(true);
+    }
+
+    const handleClose = () => {
+        setOpen(false);
+    }
 
     return(
         <div>
@@ -38,37 +54,72 @@ export default function ClassCode(props) {
                     </Typography>
                 </Alert>
             </Snackbar>
-            <IconButton onClick={(e)=>{
-                data.getState().meetingsData.forEach(e=>{
-                    if(e.className===props.className) {
-                        copyToClipboard(e.code);
-                        handleClickCopied();
-                    }
-                })
-            }}>
+
+
+            <IconButton onClick={handleOpen}>
                 <ConfirmationNumberIcon style={{color: 'white'}}/>
             </IconButton>
+
+
+
+            <Dialog
+                open={open}
+                TransitionComponent={Transition}
+                keepMounted
+                onClose={handleClose}
+                style={{backgroundColor: 'transparent', boxShadow: 'none'}}
+            >
+                <Card style={{width: 500, backgroundColor: '#2A333A', boxShadow: 'none', borderRadius: 0}}>
+                    <CardContent style={{backgroundColor: '#2A333A', borderRadius: 50}}>
+                        <Typography style={{color: 'white', fontWeight: 600, textAlign: 'center', fontSize: 30, paddingBottom: 20}}>
+                            The code for this class is:
+                        </Typography>
+
+                        <Card style={{backgroundColor: '#345f65'}}>
+                            <CardContent style={{height: 90}}>
+                                <Typography style={{color: '#d99152', fontWeight: 600, textAlign: 'center', fontSize: 40}}>
+                                    {data.getState().meetingsData.filter(e=>e.className===props.className)[0].code}
+                                </Typography>
+                            </CardContent>
+                        </Card>
+
+                        <Button variant="contained" style={{marginTop: 20, minWidth: '100%', backgroundColor: '#024873'}} onClick={()=>{
+                            // copyToClipboard(data.getState().meetingsData.filter(e=>e.className===props.className)[0].code);
+                            navigator.clipboard.writeText(data.getState().meetingsData.filter(e=>e.className===props.className)[0].code);
+                            handleClose();
+                            handleClickCopied();
+                            console.log(data.getState().meetingsData.filter(e=>e.className===props.className)[0].code)
+                        }} type='submit'>
+                            <Typography style={{color: 'white', fontWeight: 600}}>
+                                COPY TO CLIPBOARD
+                            </Typography>
+                        </Button>
+                    </CardContent>
+                </Card>
+            </Dialog>
+
+
         </div>
     )
 }
 
 
-const copyToClipboard = str => {
-    const el = document.createElement('textarea');  // Create a <textarea> element
-    el.value = str;                                 // Set its value to the string that you want copied
-    el.setAttribute('readonly', '');                // Make it readonly to be tamper-proof
-    el.style.position = 'absolute';                 
-    el.style.left = '-9999px';                      // Move outside the screen to make it invisible
-    document.body.appendChild(el);                  // Append the <textarea> element to the HTML document
-    const selected =            
-        document.getSelection().rangeCount > 0        // Check if there is any content selected previously
-        ? document.getSelection().getRangeAt(0)     // Store selection if found
-        : false;                                    // Mark as false to know no selection existed before
-    el.select();                                    // Select the <textarea> content
-    document.execCommand('copy');                   // Copy - only works as a result of a user action (e.g. click events)
-    document.body.removeChild(el);                  // Remove the <textarea> element
-    if (selected) {                                 // If a selection existed before copying
-        document.getSelection().removeAllRanges();    // Unselect everything on the HTML document
-        document.getSelection().addRange(selected);   // Restore the original selection
-    }
-};
+// const copyToClipboard = str => {
+//     const el = document.createElement('textarea');  // Create a <textarea> element
+//     el.value = str;                                 // Set its value to the string that you want copied
+//     el.setAttribute('readonly', '');                // Make it readonly to be tamper-proof
+//     el.style.position = 'absolute';                 
+//     el.style.left = '-9999px';                      // Move outside the screen to make it invisible
+//     document.body.appendChild(el);                  // Append the <textarea> element to the HTML document
+//     const selected =            
+//         document.getSelection().rangeCount > 0        // Check if there is any content selected previously
+//         ? document.getSelection().getRangeAt(0)     // Store selection if found
+//         : false;                                    // Mark as false to know no selection existed before
+//     el.select();                                    // Select the <textarea> content
+//     document.execCommand('copy');                   // Copy - only works as a result of a user action (e.g. click events)
+//     document.body.removeChild(el);                  // Remove the <textarea> element
+//     if (selected) {                                 // If a selection existed before copying
+//         document.getSelection().removeAllRanges();    // Unselect everything on the HTML document
+//         document.getSelection().addRange(selected);   // Restore the original selection
+//     }
+// };
